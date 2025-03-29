2a718402c5dd1cf06d81abd7147addfd1348888e
Rodrigo <rodrigo@mail.com>
2025-03-28 21:43:47 -0300

    Criação da função que realizar o download e compacta os pdf's

--- teste_web_scraping/web_scraping.py
+++ teste_web_scraping/web_scraping.py
@@ -1,9 +1,9 @@
 #Importando as bibliotecas necessárias
-import requests     #requisições HTTP
-from bs4 import BeautifulSoup   #Análise de conteúdo HTML
-import os   #Interação com o sistema operacional
-from urllib.parse import urljoin    #Manipulação de URLs
-import zipfile  #Manipulação de arquivos zip
+import requests     
+from bs4 import BeautifulSoup
+import os 
+from urllib.parse import urljoin 
+import zipfile 
 
 #Criando a função para identificar os links pdf do site.
 def obter_links_pdfs(url):
@@ -15,7 +15,6 @@ def obter_links_pdfs(url):
         print(f"Erro ao acessar: {e}")
         return []
     
-    #Parse na página
     dados_pagina = BeautifulSoup(pagina_pdf.text, 'html.parser')
     #Encontrando todos os links na página
     todos_links = dados_pagina.find_all('a', href=True)
@@ -31,3 +30,33 @@ def obter_links_pdfs(url):
             links_pdfs.append(href_completo)
 
     return links_pdfs
+
+
+def baixar_e_compactar_pdfs(links_pdfs):
+    # Verifica se não há links PDF encontrados
+    if not links_pdfs:
+        print("Nenhum link de PDF encontrado.")
+        return
+    
+    zip_filename = "anexos_pdfs.zip"
+    
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for link_pdf in links_pdfs:
+            try:
+                response = requests.get(link_pdf)
+                response.raise_for_status()  # Verifica se o download foi bem-sucedido
+                
+                # Mantém o nome do arquivo original
+                nome_pdf = os.path.basename(link_pdf)
+                
+                # Adicionando o conteúdo do PDF ao arquivo ZIP e tratamento de erro
+                zipf.writestr(nome_pdf, response.content)
+                print(f"Arquivo {nome_pdf} adicionado ao zip.")
+            except requests.exceptions.RequestException as e:
+                print(f"Erro ao baixar o arquivo {link_pdf}: {e}")
+
+    print(f"Arquivos PDF foram baixados e compactados em: {zip_filename}")
+
+url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+links_pdfs = obter_links_pdfs(url)
+baixar_e_compactar_pdfs(links_pdfs) 
