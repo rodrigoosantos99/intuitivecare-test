dea2f384213d563b6db74561ac9ac63d1309a61f
Rodrigo <rodrigo@mail.com>
2025-03-30 19:11:49 -0300

    Despesas ultimo trimestre

---
+++ teste_banco_dados/despesas_ultimo_trimestre.sql
@@ -0,0 +1,9 @@
+SELECT 
+    oa.razao_social AS operadora,
+    SUM(d4.vl_saldo_final) AS total_despesas
+FROM despesas_4t_2024 d4
+JOIN operadoras_ativas oa ON d4.reg_ans = oa.registro_ans
+WHERE d4.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+GROUP BY oa.razao_social
+ORDER BY total_despesas DESC
+LIMIT 10;
