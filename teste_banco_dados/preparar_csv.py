14e17ebc4277a0b26c3fca39a9225fb24b5888a4
Rodrigo <rodrigo@mail.com>
2025-03-30 18:22:17 -0300

    Melhorando código de preparar csv

--- teste_banco_dados/preparar_csv.py
+++ teste_banco_dados/preparar_csv.py
@@ -1,28 +1,38 @@
 from pathlib import Path
 import pandas as pd
 import zipfile
+import os
 
-def concatenar_csv_de_zips(arquivos_zip):
+def concatenar_csv_de_zip(zip_path):
     dfs = []
-
-    for zip_path in arquivos_zip:
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            for nome_arquivo in zip_ref.namelist():
-                with zip_ref.open(nome_arquivo) as arquivo:
-                    df = pd.read_csv(arquivo, encoding="utf-8", sep=None, engine="python")
-                    dfs.append(df)
-
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        for nome_arquivo in zip_ref.namelist():
+            with zip_ref.open(nome_arquivo) as arquivo:
+                df = pd.read_csv(arquivo, encoding="utf-8", sep=None, engine="python")
+                for col in df.select_dtypes(include=["object"]).columns:
+                    df[col] = df[col].replace({',': '.'}, regex=True)
+                dfs.append(df)
+    
     df_final = pd.concat(dfs, ignore_index=True)
-    caminho_saida = diretorio_atual/"despesas_2023_2024.csv"
-    df_final.to_csv(caminho_saida, index=False, encoding="utf-8")
+    
+    return df_final
 
-    print(f"Arquivo concatenado salvo em: {caminho_saida}")
+def extrair_arquivos_zip(zip_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(Path(__file__).parent)
+    print(f"Arquivos extraídos para o diretório atual: {Path(__file__).parent}")
 
+def processar_arquivos():
+    diretorio_atual = Path(__file__).parent
 
-diretorio_atual = Path(__file__).parent
+    zip_2023 = diretorio_atual / "2023.zip"
+    df_2023 = concatenar_csv_de_zip(zip_2023)
 
-zip_arquivos = [diretorio_atual/"2023.zip", diretorio_atual/"2024.zip"]
-concatenar_csv_de_zips(zip_arquivos)
+    zip_2024 = diretorio_atual / "2024.zip"
+    extrair_arquivos_zip(zip_2024)
 
-
-
+    caminho_saida = diretorio_atual / "despesas_2023.csv"
+    df_2023.to_csv(caminho_saida, index=False, encoding="utf-8", sep=";")
+    print(f"Arquivo concatenado de 2023 salvo em: {caminho_saida}")
+    
+processar_arquivos()
