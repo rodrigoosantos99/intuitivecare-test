35de71a389c33ebe64dc1b713116565aa707f7a9
Rodrigo <rodrigo@mail.com>
2025-03-30 22:32:37 -0300

    Funcao princiapal para o script web scrapping

--- teste_web_scraping/web_scraping.py
+++ teste_web_scraping/web_scraping.py
@@ -4,7 +4,6 @@ import os
 from urllib.parse import urljoin 
 import zipfile 
 
-#Criando a função para identificar os links pdf do site.
 def obter_links_pdfs(url):
     try:
         pagina_pdf = requests.get(url)
@@ -15,7 +14,6 @@ def obter_links_pdfs(url):
     
     dados_pagina = BeautifulSoup(pagina_pdf.text, 'html.parser')
     
-    #Encontrando todos os links na página
     todos_links = dados_pagina.find_all('a', href=True)
     
     links_pdfs = []
@@ -29,10 +27,7 @@ def obter_links_pdfs(url):
     return links_pdfs
 
 def filtrar_pdfs_desejados(links_pdfs):
-    links_pdfs_filtrados = []
-    for link in links_pdfs:
-        if "anexo" in link.lower():
-            links_pdfs_filtrados.append(link)
+    links_pdfs_filtrados = [link for link in links_pdfs if "anexo" in link.lower()]
     if not links_pdfs_filtrados:
         print("Nenhum PDF encontrado.")
     return links_pdfs_filtrados
@@ -50,18 +45,19 @@ def baixar_e_compactar_pdfs(links_pdfs):
                 response = requests.get(link_pdf)
                 response.raise_for_status()
                 
-                # Mantém o nome do arquivo original
                 nome_pdf = os.path.basename(link_pdf)
-                
-                # Adicionando o conteúdo do PDF ao arquivo ZIP e tratamento de erro
                 zipf.writestr(nome_pdf, response.content)
                 print(f"Arquivo {nome_pdf} adicionado ao zip.")
             except requests.exceptions.RequestException as e:
                 print(f"Erro ao baixar o arquivo {link_pdf}: {e}")
-
+    
     print(f"Arquivos PDF foram baixados e compactados em: {zip_filename}")
 
-url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
-links_pdfs = obter_links_pdfs(url)
-links_pdfs_filtrados = filtrar_pdfs_desejados(links_pdfs)
-baixar_e_compactar_pdfs(links_pdfs_filtrados)
+def funcao_principal():
+    url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+    links_pdfs = obter_links_pdfs(url)
+    links_pdfs_filtrados = filtrar_pdfs_desejados(links_pdfs)
+    baixar_e_compactar_pdfs(links_pdfs_filtrados)
+
+
+funcao_principal()
